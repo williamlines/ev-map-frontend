88f083611a5f39384dd05ccf387249f95bcb4b58
William Lines <williamlines42@gmail.com>
2023-02-06 15:38:28 +0000

    can now use lambda with getDirections method

--- src/api_calls/directions.js
+++ src/api_calls/directions.js
@@ -1,10 +1,11 @@
-async function getDirections(lat, lng) {
+async function getDirections(origin, destination) {
   const res = await fetch(
-    "https://q7bxcdafs9.execute-api.eu-west-2.amazonaws.com/prod/directions"
+    `https://q7bxcdafs9.execute-api.eu-west-2.amazonaws.com/prod/directions?origin=${origin}&destination=${destination}`
   );
   const data = await res.json();
   console.log(data);
+  console.log(data.message.routes[0].geometry.coordinates);
   return;
 }
-
-getDirections(1, 2);
+// getDirections("-84.518641%2C39.134270", "-84.512023%2C39.102779");
+module.exports = getDirections()
