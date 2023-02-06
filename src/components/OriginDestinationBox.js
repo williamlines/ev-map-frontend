003fedc9fcbdbf4302e5e45ea4d6594b97b5f615
William Lines <williamlines42@gmail.com>
2023-02-06 16:40:03 +0000

    started to add origin and destination setting

--- src/components/OriginDestinationBox.js
+++ src/components/OriginDestinationBox.js
@@ -1,9 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Button, ButtonGroup, HStack, Text } from "@chakra-ui/react";
 
 const OriginDestinationBox = () => {
+  const [originSelect, setOriginSelect] = useState(false);
+  const [origin, setOrigin] = useState(null);
+  const [destinationSelect, setdestinationSelect] = useState(false);
+  const [destination, setDestination] = useState(null);
   return (
     <Box p={4} borderRadius="lg" bgColor="tomato">
-      some text
+      <HStack>
+        <Button colorScheme="blue">Select Origin</Button>
+        <Text> {`Origin is ${origin}`} </Text>
+      </HStack>
+      <HStack>
+        <Button colorScheme="green">Select Destination</Button>
+        <Text> {`Destination is ${destination}`} </Text>
+      </HStack>
     </Box>
   );
 };
