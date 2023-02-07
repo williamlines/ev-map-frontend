fc7c39b0ad0938ec08b885f9f3e4fa5578616072
William Lines <williamlines42@gmail.com>
2023-02-07 16:27:23 +0000

    test drove formatLngLat method

--- src/functions/formatLngLat.js
+++ src/functions/formatLngLat.js
@@ -1,4 +1,8 @@
-const formatLngLat = () => {
+const formatLngLat = (input) => {
+  const tempArray = input.split(", ");
+  tempArray[0] = tempArray[0].slice(7);
+  tempArray[1] = tempArray[1].slice(0, -1);
+  return tempArray.join("%2C");
 };
 
 module.exports = formatLngLat;
